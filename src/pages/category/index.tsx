695f5885a025318e32c7181a30a3077084dc6471
Jhon <jhondharkyson@gmail.com>
2025-07-28 06:38:02 -0300

    Refactor: Finish refactor of category

--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -1,32 +1,27 @@
-import Head from "next/head"
-import { Header } from '../../components/Header'
-import styles from './style.module.scss'
-import {useState, FormEvent} from 'react'
-import {setupAPIClient} from '../../services/api'
-import {toast} from 'react-toastify'
-import {canSSRAuth} from '../../utils/canSSRAuth'
+import Head from "next/head";
+import { Header } from '../../components/Header';
+import styles from './style.module.scss';
+import {useState, FormEvent} from 'react';
+import {setupAPIClient} from '../../services/api';
+import {toast} from 'react-toastify';
+import {canSSRAuth} from '../../utils/canSSRAuth';
 
-export default function Category(){
-
+export default function Category() {
     const [name, setName] = useState('');
 
-    async function handleRegister(event: FormEvent){
-
+    async function handleRegister(event: FormEvent) {
         event.preventDefault();
-        if(name === ''){
+
+        if(name === '') {
             return(
                 toast.warning('Preencha o campo vazio para cadastrar!')
             );
         }
 
         const apiClient = setupAPIClient();
-        await apiClient.post('/category',{
-            name: name
-        })
-
+        await apiClient.post('/category/create', {name: name});
         toast.success('Categoria cadastrada com sucesso!');
         setName('');
-
     }
 
     return (
@@ -35,20 +30,17 @@ export default function Category(){
         <Head>
             <title>Nova categoria - Sujeito Pizzaria</title>
         </Head>
-        <div>
-            
+        <div>            
             <main className={styles.container}>
                 <h1>Cadastrar categoria</h1>
-
                 <form className={styles.form} onSubmit={handleRegister}>
-                    <input type="text" 
-                    placeholder="Digite o nome da categoria"
-                    className={styles.input}
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    
+                    <input 
+                        type="text" 
+                        placeholder="Digite o nome da categoria"
+                        className={styles.input}
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}                    
                     />
-
                     <button className={styles.buttonAdd} type="submit">
                         Cadastrar
                     </button>
@@ -56,13 +48,11 @@ export default function Category(){
             </main>
         </div>
         </>
-    )
+    );
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
-
     return {
         props:{}
     }
-
 });
